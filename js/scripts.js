8136fb1501c2d87a29a2a302155d18a28629fe43
Kris Hopper <hopperdavis@gmail.com>
2021-06-01 14:49:42 -0700

    practice jquery

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  // $("h1").click(function () {
+  //   alert("This is a heading.");
+  // });
+
+  // $("p").click(function () {
+  //   alert("This is a paragraph.");
+  // });
+
+  // $("img").click(function () {
+  //   alert("This is an image.");
+  // });
+  $("p").click(function() {
+    $(".walrus-showing").show();
+  });
+});
